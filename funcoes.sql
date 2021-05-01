6b837758b4048ad2c8bfb86788ceb0130e7a5d8b
Patrick <patrick.oliveira@aluno.ufabc.edu.br>
2021-05-01 17:02:52 -0300

    totais globais

--- funcoes.sql
+++ funcoes.sql
@@ -161,10 +161,6 @@ RETURNS TABLE (
 	END
 $$ LANGUAGE plpgsql;
 
-
-
-			   
-
 CREATE OR REPLACE FUNCTION municipios_por_estado(uf_sigla char(2))
 RETURNS TABLE (
 				codigo int,
@@ -185,6 +181,246 @@ RETURNS TABLE (
 	END;
 $$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION informacoes_totais_globais()
+RETURNS TABLE (
+				codigo int,
+				pais varchar(100),
+				casos int,
+				mortes int,
+				testes int,
+				vacinacoes int,
+				hospitalizacoes int
+			   ) AS $$
+			   
+	BEGIN
+		RETURN QUERY
+			SELECT  informacoes_totais_globais.paisid, informacoes_totais_globais.nome, 
+					MAX(total_casos) as total_casos, 
+					MAX(total_mortes) as total_mortes, 
+					MAX(total_testes) as total_testes, 
+					MAX(total_vacinacoes) as total_vacinacoes, 
+					MAX(total_hospitalizacoes) as total_hospitalizacoes
+			FROM (
+				SELECT * FROM 
+					(SELECT informacoes_globais_casos.paisid, datas, quantidade_total AS total_casos 
+					 FROM informacoes_globais_casos) 
+						AS casos
+					NATURAL JOIN 
+					(SELECT informacoes_globais_mortes.paisid, datas, quantidade_total AS total_mortes 
+					 FROM informacoes_globais_mortes)
+						AS mortes
+					NATURAL JOIN
+					(SELECT informacoes_globais_testes.paisid, datas, quantidade_total AS total_testes 
+					 FROM informacoes_globais_testes)
+						AS testes
+					NATURAL JOIN
+					(SELECT informacoes_globais_vacinacoes.paisid, datas, quantidade_total AS total_vacinacoes 
+					 FROM informacoes_globais_vacinacoes)
+						AS vacinacoes
+					NATURAL JOIN
+					(SELECT informacoes_globais_hospitalizacoes.paisid, datas, quantidade_total AS total_hospitalizacoes 
+					 FROM informacoes_globais_hospitalizacoes)
+						AS hospitalizacoes
+					INNER JOIN
+					ente_federativo
+						ON ente_federativo.codigo = casos.paisid
+			) AS informacoes_totais_globais
+			GROUP BY paisid, nome;
+	END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION total_registros_estados()
+RETURNS TABLE (
+				UF varchar(2),
+				total_ocupacao_suspeita int,
+				total_ocupacao_confirmado int,
+				total_UTI_suspeita int,
+				total_UTI_confirmado int,
+				total_obitos_suspeita int,
+				total_obitos_confirmado int,
+				total_altas_suspeita int,
+				total_altas_confirmado int
+			   ) AS $$
+			   
+	BEGIN
+		RETURN QUERY
+			with registros_estado as (
+				select estado.sigla, dataregistro, clinica_ocup_suspeita, clinica_ocup_confirmado,
+					   uti_ocup_suspeita, uti_ocup_confirmado, obitos_suspeita, obitos_confirmado,
+					   alta_suspeita, alta_confirmado
+					from 
+						estado inner join endereco
+							on estado.codigo = endereco.estado
+						natural join estabelecimento_de_saude
+						inner join registro_de_ocupacao 
+							on estabelecimento_de_saude.cnes = registro_de_ocupacao.cnes
+						inner join registro
+							on registro.codigo = registro_de_ocupacao.codigo
+			)
+
+			select  registros_estado.sigla,
+					sum(registros_estado.clinica_ocup_suspeita)::int 
+						as total_clinica_ocup_suspeita,
+					sum(registros_estado.clinica_ocup_confirmado)::int 
+						as total_clinica_ocup_confirmado,
+					sum(registros_estado.uti_ocup_suspeita)::int 
+						as total_uti_ocup_suspeita,
+					sum(registros_estado.uti_ocup_confirmado)::int 
+						as total_uti_ocup_confirmado,
+					sum(registros_estado.obitos_suspeita)::int 
+						as total_obitos_suspeita,
+					sum(registros_estado.obitos_confirmado)::int 
+						as total_obitos_confirmado,
+					sum(registros_estado.alta_suspeita)::int 
+						as total_alta_suspeita,
+					sum(registros_estado.alta_confirmado)::int 
+						as total_alta_confirmado
+			from registros_estado
+			group by sigla;
+	END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION total_registros_municipios(UF_input char(2))
+RETURNS TABLE (
+				nome varchar(100),
+				total_ocupacao_suspeita int,
+				total_ocupacao_confirmado int,
+				total_UTI_suspeita int,
+				total_UTI_confirmado int,
+				total_obitos_suspeita int,
+				total_obitos_confirmado int,
+				total_altas_suspeita int,
+				total_altas_confirmado int
+			   ) AS $$
+			   
+	BEGIN
+		RETURN QUERY
+			WITH registros_municipio AS (
+				SELECT  ente_federativo.nome, 
+						registro.dataregistro, registro.clinica_ocup_suspeita,
+						registro.clinica_ocup_confirmado,
+						registro.uti_ocup_suspeita, registro.uti_ocup_confirmado,
+						registro.obitos_suspeita, registro.obitos_confirmado,
+						registro.alta_suspeita, registro.alta_confirmado
+					FROM estado
+					INNER JOIN municipio
+						ON municipio.estadoid = estado.codigo
+					INNER JOIN ente_federativo
+						ON municipio.codigo = ente_federativo.codigo
+					INNER JOIN endereco
+						ON municipio.codigo = endereco.municipio
+					INNER JOIN estabelecimento_de_saude
+						ON endereco.cep = estabelecimento_de_saude.cep
+					INNER JOIN registro_de_ocupacao
+						ON estabelecimento_de_saude.cnes = registro_de_ocupacao.cnes
+					INNER JOIN registro
+						ON registro_de_ocupacao.codigo = registro.codigo
+					WHERE estado.sigla = UF_input
+			)
+			
+			SELECT registros_municipio.nome,
+					sum(registros_municipio.clinica_ocup_suspeita)::int 
+						as total_clinica_ocup_suspeita,
+					sum(registros_municipio.clinica_ocup_confirmado)::int 
+						as total_clinica_ocup_confirmado,
+					sum(registros_municipio.uti_ocup_suspeita)::int 
+						as total_uti_ocup_suspeita,
+					sum(registros_municipio.uti_ocup_confirmado)::int 
+						as total_uti_ocup_confirmado,
+					sum(registros_municipio.obitos_suspeita)::int 
+						as total_obitos_suspeita,
+					sum(registros_municipio.obitos_confirmado)::int 
+						as total_obitos_confirmado,
+					sum(registros_municipio.alta_suspeita)::int 
+						as total_alta_suspeita,
+					sum(registros_municipio.alta_confirmado)::int 
+						as total_alta_confirmado
+			from registros_municipio
+			group by registros_municipio.nome;
+	END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION media_movel_casos_pais(nome_pais varchar(100))
+RETURNS TABLE (
+				datas date,
+				novos int,
+				media_movel decimal
+			   ) AS $$
+	BEGIN
+		RETURN QUERY
+			WITH paises_info AS (
+				SELECT codigo, sigla, nome FROM pais NATURAL JOIN ente_federativo
+			)
+
+			SELECT informacoes_globais_casos.datas, informacoes_globais_casos.novos,
+				   AVG(informacoes_globais_casos.novos) OVER(ORDER BY informacoes_globais_casos.datas 
+				   										ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)
+			FROM 
+				(SELECT codigo FROM paises_info WHERE nome = nome_pais) AS codigo_pais
+				INNER JOIN informacoes_globais_casos
+					ON codigo_pais.codigo = informacoes_globais_casos.paisid;
+					
+	END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION media_movel_mortes_pais(nome_pais varchar(100))
+RETURNS TABLE (
+				datas date,
+				novos int,
+				media_movel decimal
+			   ) AS $$
+	BEGIN
+		RETURN QUERY
+			WITH paises_info AS (
+				SELECT codigo, sigla, nome FROM pais NATURAL JOIN ente_federativo
+			)
+
+			SELECT informacoes_globais_mortes.datas, informacoes_globais_mortes.novos,
+				   AVG(informacoes_globais_mortes.novos) OVER(ORDER BY informacoes_globais_mortes.datas 
+				   										ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)
+			FROM 
+				(SELECT codigo FROM paises_info WHERE nome = nome_pais) AS codigo_pais
+				INNER JOIN informacoes_globais_mortes
+					ON codigo_pais.codigo = informacoes_globais_mortes.paisid;
+				
+	END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION media_movel_paises()
+RETURNS TABLE (
+				nomes varchar(30),
+				datas date,
+				novos_casos int,
+				media_movel_casos decimal,
+				novas_mortes int,
+				media_movel_mortes decimal
+			   ) AS $$
+	BEGIN
+		RETURN QUERY
+			WITH paises_info AS (
+				SELECT codigo, sigla, nome FROM pais NATURAL JOIN ente_federativo
+			)
+
+			SELECT codigo_pais.nome, informacoes_globais_casos.datas, 
+				   informacoes_globais_casos.novos,
+				   AVG(informacoes_globais_casos.novos) OVER(PARTITION BY codigo_pais.nome 
+				   										ORDER BY informacoes_globais_casos.datas 
+				   										ROWS BETWEEN 6 PRECEDING AND CURRENT ROW),
+				   informacoes_globais_mortes.novos,
+				   AVG(informacoes_globais_mortes.novos) OVER(PARTITION BY codigo_pais.nome 
+				   										 ORDER BY informacoes_globais_mortes.datas 
+				   										 ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)
+			FROM 
+				(SELECT codigo, nome FROM paises_info) AS codigo_pais
+				INNER JOIN informacoes_globais_mortes
+				ON codigo_pais.codigo = informacoes_globais_mortes.paisid
+				INNER JOIN informacoes_globais_casos
+				ON informacoes_globais_mortes.paisid = informacoes_globais_casos.paisid
+			
+			WHERE informacoes_globais_mortes.datas = informacoes_globais_casos.datas;
+				
+	END
+$$ LANGUAGE plpgsql;
 			 
 -- SELECT * FROM registros_por_estado('Minas Gerais');
 -- SELECT * FROM municipios_por_estado('AC');
@@ -193,13 +429,19 @@ $$ LANGUAGE plpgsql;
 -- SELECT * FROM informacoes_mortes_por_pais('Brazil');
 -- SELECT * FROM informacoes_testes_por_pais('Brazil');
 -- SELECT * FROM informacoes_vacinacao_por_pais('Brazil');
+-- SELECT * FROM registros_por_estado('SP');
+-- SELECT * FROM media_movel_casos_pais('Brazil');
+SELECT * FROM media_movel_mortes_pais('Brazil');
+-- select * from registros_por_municipio('São Paulo');
 
-WITH informacoes_casos_pais AS(
-	SELECT * FROM informacoes_casos_por_pais('Brazil')
-)
+-- select * from total_registros_estados();
+-- SELECT * FROM total_registros_municipios('SP');
+-- SELECT * FROM informacoes_totais_globais();
 
-SELECT datas,
-	AVG(novos) OVER(ORDER BY datas
-		 		    ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)
-	AS movin_average
-FROM informacoes_casos_pais;
+
+
+
+
+
+
+
